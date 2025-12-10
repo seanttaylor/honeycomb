770be5a9c9d1aa501e7d58fb288493dd48e1ce56
Sean Travis Taylor <seantravistaylor@Seans-Macbook-Air-EOS.lan>
2025-12-10 18:32:39 -0500

    SCAFFOLDED HTTP SERVER IN NOOPService

--- docker/noop/index.js
+++ docker/noop/index.js
@@ -1,4 +1,11 @@
 import { HC2Proxy, HC2Utilities } from '@honeycomb/cloud';
+import bodyParser from 'body-parser';
+import express from 'express';
+import http from 'http';
+import morgan from 'morgan';
+import Ajv from 'ajv';
+import { NOOPService } from './service.js';
+
 /**
   * @returns {Object}
   */
@@ -43,53 +50,118 @@ async function signPayload(payload, privateKey) {
       payload,
       signature: HC2Utilities.ArrayBuffer.toBase64(signature),
     };
-  }
+}
 
 (async function HC2NOOPService() {
-    /** CONTAINER ENVIRONMENT VARIABLES **/
+    /******** CONTAINER ENVIRONMENT VARIABLES ********/
     const SERVICE_NAME = process.env.SERVICE_NAME;
     const VERSION = process.env.VERSION;
     const HC2_INSTANCE_URL = process.env.HC2_INSTANCE_URL;
+    const PORT = process.env.PORT || 3001;
     const HC2_SERVICE_CERTIFICATE = JSON.parse(atob(process.env.HC2_SERVICE_CERTIFICATE));
     
-    const { publicKey: PUBLIC_KEY, privateKey: PRIVATE_KEY } = await generateKeyPair();
-    const serviceRegistrationRequest = {
-        app: 'current.ly',
-        service: 'NOOPService',
-        version: 1,
-        api: {
-          methods: [
-            {
-              name: 'hello',
-              params: {
-                type: 'object',
-                properties: {
-                  receiver: {
-                    type: 'string',
-                  },
-                  sender: {
-                    type: 'string',
-                  },
-                },
-                required: ['receiver'],
-                additionalProperties: false,
-              },
-            },
-          ],
-        },
-        callbackURL: 'http://foo.bar.baz.cloud',
-        HC2ServiceCertificate: HC2_SERVICE_CERTIFICATE
-    };
-    const SERVICE_SIGNED_REGISTRATION_REQ = await signPayload(serviceRegistrationRequest, PRIVATE_KEY);
-
     try {
+        const { publicKey: PUBLIC_KEY, privateKey: PRIVATE_KEY } = await generateKeyPair();
+        const serviceRegistrationRequest = {
+            app: 'current.ly',
+            service: 'NOOPService',
+            version: 1,
+            api: {
+            methods: [
+                {
+                name: 'hello',
+                params: {
+                    type: 'object',
+                    properties: {
+                    receiver: {
+                        type: 'string',
+                    },
+                    sender: {
+                        type: 'string',
+                    },
+                    },
+                    required: ['receiver'],
+                    additionalProperties: false,
+                },
+                },
+            ],
+            },
+            callbackURL: 'http://foo.bar.baz.cloud',
+            HC2ServiceCertificate: HC2_SERVICE_CERTIFICATE
+        };
+        const SERVICE_SIGNED_REGISTRATION_REQ = await signPayload(serviceRegistrationRequest, PRIVATE_KEY);
+
         console.info(`${SERVICE_NAME} v${VERSION}`);
+        
         const hc2 = new HC2Proxy(HC2_INSTANCE_URL);
+        const noopService = new NOOPService(hc2);
         const hc2ServiceRegistrationReceipt = await hc2.register(SERVICE_SIGNED_REGISTRATION_REQ);
-        const reply = await hc2.my.FeedService.hello({ from: SERVICE_NAME });
-        
-        console.log({ reply });
-        
+
+        //const reply = await hc2.my.FeedService.hello({ from: SERVICE_NAME });
+
+        /******** HTTP SERVER ********/
+        const app = express();
+        app.use(bodyParser.json());
+        app.use(morgan('combined'));
+
+        app.get('/health', (req, res) => {
+            const serviceStatus = noopService.status;
+            res.status(200).json(serviceStatus);
+        });
+
+        app.post('/rpc', async (req, res) => {
+            try {
+                const ajv = new Ajv();
+                const { name, params: methodParams } = req.body;
+                const schema = serviceRegistrationRequest.api.methods.find((el, idx) => {
+                    return el.name === name;
+                });
+
+                if (!schema) {
+                    console.error(`INTERNAL_ERROR (${SERVICE_NAME}): Service method (${name})`);
+                    res.set('content-type', 'application/problem+json');
+                    res.status(404).json({
+                        type: 'https://hc2.io/probs/invalid-service-method',
+                        title: `The service method specified does not exist on target service`,
+                        instance: `/requests/${crypto.randomUUID()}`,
+                    });
+                    return;
+                }
+
+                const validation = ajv.validate(schema.params, methodParams);
+
+                if (!validation) {
+                    console.error(`INTERNAL_ERROR (${SERVICE_NAME}): Invalid params for service method (${schema.name})`);
+                    res.set('content-type', 'application/problem+json');
+                    res.status(404).json({
+                        type: 'https://hc2.io/probs/invalid-service-method',
+                        title: `The service method specified does not exist on ${SERVICE_NAME}`,
+                        detail: `${JSON.stringify(validation.errors)}`,
+                        instance: `/requests/${crypto.randomUUID()}`,
+                    });
+                    return;
+                }
+
+                const response = await noopService[name](methodParams);
+                res.status(200).json({
+                    message: response
+                });
+            } catch(ex) {
+                console.error(`INTERNAL_ERROR (NOOPService): **EXCEPTION ENCOUNTERED** while executing service method. See details -> ${ex.message}`);
+                next(ex);
+            }
+        });
+
+        app.use((err, req, res, next) => {
+            const status = err.status || 500;
+            console.error(err);
+            res.status(status).send({ status, error: 'There was an error.' });
+        });
+
+        http.createServer(app).listen(PORT, () => {
+            console.log(`honeycomb.HC2 service (${SERVICE_NAME}) listening on port ${PORT}`);
+        });
+                
     } catch(ex) {
         console.error(`INTERNAL_ERROR (HC2.Service): **EXCEPTION ENCOUNTERED** while running the HC2 service (${SERVICE_NAME}). See details -> ${ex.message}` );
     }
---
+++ docker/noop/service.js
@@ -0,0 +1,51 @@
+export class ApplicationService {
+    constructor() {}
+  
+    get status() {
+      return {
+        name: this.constructor.name,
+        timestamp: new Date().toISOString(),
+      };
+    }
+  
+}
+
+/**
+ * This service is just used as a sanity check to ensure
+ * the module system is working
+ */
+export class NOOPService extends ApplicationService {
+    #logger;
+    #sandbox;
+    #HC2_PROXY;
+  
+    /**
+     * @param {Object} hc2Proxy
+     */
+    constructor(hc2Proxy) {
+      super();
+  
+      //this.#sandbox = sandbox;
+      //this.#logger = sandbox.core.logger.getLoggerInstance();
+  
+      try {
+        this.#HC2_PROXY = hc2Proxy;
+      } catch (ex) {
+        console.error(
+          `INTERNAL_ERROR (NOOPService): Exception encountered while starting the service. See details -> ${ex.message}`
+        );
+      }
+    }
+    
+    /**
+     * @param {Object} options
+     * @param {String} options.receiver
+     * @param {String} options.sender
+     * @returns {String}
+     */
+    async hello({ receiver, sender }) {
+      return `[${this.constructor.name}] has a message for ${receiver} from: ${
+        sender ? sender : 'Someone special'
+      }`;
+    }
+}
